18d9c12c0ad8d9693960595267e4d98cb9d81abe
Michael Gilliland <mjg.py3@gmail.com>
2015-08-14 22:48:16 -0400

    Made it compile!

--- src/Language/PureScript/CodeGen/Lisp.hs
+++ src/Language/PureScript/CodeGen/Lisp.hs
@@ -4,24 +4,28 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Language.PureScript.CodeGen.Lisp
-  ( module AST
-  , moduleToLisp
+  ( moduleToLisp
   ) where
 
-import Language.PureScript.CodeGen.Lisp.AST as AST
+import Control.Applicative
+import Control.Monad.Reader (MonadReader)
+import Control.Monad.Supply.Class
+
 import Language.PureScript.CoreFn
 import Language.PureScript.Names
 import Language.PureScript.Options
 import Language.PureScript.Traversals (sndM)
 import qualified Language.PureScript.Constants as C
 
-data LispDialect = LispDialect {
+data DialectSpecifics = DialectSpecifics {
 }
+
+data Lisp = Lisp
 
 -- |
 -- Generate code in the simplified Lisp intermediate representation for all declarations in a
 -- module.
 --
 moduleToLisp :: forall m. (Applicative m, Monad m, MonadReader Options m, MonadSupply m)
-           => Module Ann -> Maybe Lisp -> LispDialect -> m [Lisp]
+           => Module Ann -> Maybe Lisp -> DialectSpecifics -> m [Lisp]
 moduleToLisp (Module coms mn imps exps foreigns decls) foreign dialect = undefined
